a57dfa0a7498f2682e31db2782677cf5e6638cb9
Dilip9 <dilip.verma940805@gmail.com>
2026-01-05 23:09:24 +0530

    Initial Commit for notification-tracker

---
+++ src/test/java/gouri/notification/notification_tracker/NotificationTrackerApplicationTests.java
@@ -0,0 +1,13 @@
+package gouri.notification.notification_tracker;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class NotificationTrackerApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
